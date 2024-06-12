6351bd35a9ccd6d95771eb0da0e3398da09f3756
vikram1503 <vikram15031989@gmail.com>
2024-06-12 20:55:09 +0530

    tools

---
+++ cicd/main.tf
@@ -0,0 +1,96 @@
+module "jenkins" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins-tf"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-07832342d5cdd6bb4"] #replace your SG
+  subnet_id = "subnet-047891b2b720a7614" #replace your Subnet
+  ami = data.aws_ami.ami_info.id
+  user_data = file("jenkins.sh")
+  tags = {
+    Name = "jenkins-tf"
+  }
+}
+
+module "jenkins_agent" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins-agent"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-07832342d5cdd6bb4"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-047891b2b720a7614"
+  ami = data.aws_ami.ami_info.id
+  user_data = file("jenkins-agent.sh")
+  tags = {
+    Name = "jenkins-agent"
+  }
+}
+
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-07832342d5cdd6bb4"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-047891b2b720a7614"
+  ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "jenkins"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.jenkins.public_ip
+      ]
+    },
+    {
+      name    = "jenkins-agent"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.jenkins_agent.private_ip
+      ]
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip
+      ]
+    }
+  ]
+
+}
---
+++ cicd/variables.tf
@@ -0,0 +1,5 @@
+variable "zone_name" {
+  type        = string
+  default     = "imvicky.online"
+  description = "description"
+}
