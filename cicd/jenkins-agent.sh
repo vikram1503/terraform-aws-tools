f59f14f6cd799e66e6c8e05e2d796a35078c9358
vikram1503 <vikram15031989@gmail.com>
2024-07-22 11:41:00 +0530

    practice

--- cicd/jenkins-agent.sh
+++ cicd/jenkins-agent.sh
@@ -21,3 +21,8 @@ usermod -aG docker ec2-user
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+# kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
